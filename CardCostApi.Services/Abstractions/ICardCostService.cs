45fbd240a70e2c67fa9b0393cb6bc09d59380ff5
arisfan <aristeidis.fanaras@betssongroup.com>
2022-12-31 16:21:53 +0100

    Refactoring folder names.

--- CardCostApi.Services/Abstractions/ICardCostService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace CardCostApi.Core.Abstractions
-{
-    public interface ICardCostService
-    {
-        Task<(decimal cost, string country)> GetCardCost(string bin);
-    }
-}
--- CardCostApi.Services/Settings/DefaultCardCostSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CardCostApi.Core.Settings
-{
-    public class DefaultCardCostSettings
-    {
-        public decimal Cost { get; set; }
-        public string Country { get; set; }
-    }
-}
